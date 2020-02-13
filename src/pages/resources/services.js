aafc07b37e37a93769a68bc7f08a64f4f82f0202
Matt Watson <mattwat@gmail.com>
2020-02-12 21:09:38 -0500

    rearrange service card content

--- src/pages/resources/services.js
+++ src/pages/resources/services.js
@@ -3,17 +3,21 @@ import styled from 'styled-components'
 import { Link } from 'gatsby'
 import { SEO } from '../../components/seo'
 import { PageContent } from '../../components/layout'
-import { Title, Heading, Paragraph } from '../../components/typography'
+import { Title, Heading, Subheading, Paragraph } from '../../components/typography'
 import { Card, CardHeader, CardBody } from '../../components/card'
 import { Container as Grid, Row, Col, Visible } from 'react-grid-system'
 import { ExternalLink } from '../../components/link'
 
-const tools = [
+const services = [
     {
-        title: 'Explore Available Data',
-        items: [
+        cardTitle: 'Explore Available Data',
+        cardItems: [
             {
-                description: 'Use BioData Catalyst powered by Gen3 to discover and learn more about variables in TOPMed projects and to apply for access through dbGaP. Gen3 also allows for search through hosted data and for creating synthetic cohorts of interest and exporting the data to an analysis environment.',
+                platform: 'Gen3',
+                description: `
+                    Use this platform to discover and learn more about variables in TOPMed projects and to apply for access through dbGaP.
+                    Gen3 also allows for search through hosted data and for creating synthetic cohorts of interest and exporting the data to an analysis environment.
+                `,
                 links: {
                     launch: 'https://gen3.datastage.io/',
                     documentation: '',
@@ -21,7 +25,11 @@ const tools = [
                 },
             },
             {
-                description: 'Explore available data through BioData Catalyst powered by PIC-SURE with interactive search and visualizations for feasibility assessment. Use query results to create a cohort, with the ability to choose specific variables of interest to export into an analysis environment. ',
+                platform: 'PIC-SURE',
+                description: `
+                    Explore available data through BioData Catalyst powered by PIC-SURE with interactive search and visualizations for feasibility assessment.
+                    Use query results to create a cohort, with the ability to choose specific variables of interest to export into an analysis environment.
+                `,
                 links: {
                     launch: 'https://biodatacatalyst.integration.hms.harvard.edu/psamaui/login/?redirection_url=/picsureui/',
                     documentation: '',
@@ -31,10 +39,15 @@ const tools = [
         ]
     },
     {
-        title: 'Analyze Data in Cloud-based Shared Workspaces',
-        items: [
+        cardTitle: 'Analyze Data in Cloud-based Shared Workspaces',
+        cardItems: [
             {
-                description: 'BioData Catalyst powered by Seven Bridges offers collaborative workspaces for analyzing genomics data at scale. Access hosted datasets along with Common Workflow Language pipelines for analysis. This platform also  enables users to bring their own data for analysis and work in RStudio and Jupyterlab Notebooks for interactive analysis.',
+                platform: 'Seven Bridges',
+                description: `
+                    With this platform users can utilize collaborative workspaces for analyzing genomics data at scale.
+                    Access hosted datasets along with Common Workflow Language pipelines for analysis.
+                    This platform also enables users to bring their own data for analysis and work in RStudio and Jupyterlab Notebooks for interactive analysis.
+                `,
                 links: {
                     launch: 'https://f4c-accounts.sbgenomics.com/auth/login?next=https%3A%2F%2Ff4c-accounts.sbgenomics.com%2Foauth2%2Fauthorization%3Fresponse_type%3Dcode%26client_id%3D74735b722c33498cb981b6b6e24f659f%26redirect_uri%3Dhttps%253A%252F%252Ff4c.sbgenomics.com%252Foauth2%252Fredirect%26scope%3Dopenid%26state%3DD9lNrFc8pMySUALiuvewZkO0LGl8oc%26nonce%3D111038105642055197041580917835',
                     documentation: '',
@@ -42,7 +55,11 @@ const tools = [
                 },
             },
             {
-                description: 'BioData Catalyst powered by Terra is a scalable cloud-based platform for democratizing genomic data access and for sharing and computing across large genomic and genomic-related data sets. Terra offers a stand-alone computational workspace model that provides a secure collaborative place to organize data, run and monitor analysis pipelines, and perform interactive analysis using applications such as Jupyter Notebooks.',
+                platform: 'Terra',
+                description: `
+                    This scalable cloud-based platform can be used for democratizing genomic data access and for sharing and computing across large genomic and genomic-related data sets.
+                    Terra offers a stand-alone computational workspace model that provides a secure collaborative place to organize data, run and monitor analysis pipelines, and perform interactive analysis using applications such as Jupyter Notebooks.
+                `,
                 links: {
                     launch: 'https://datastage.terra.bio/',
                     documentation: '',
@@ -52,10 +69,14 @@ const tools = [
         ]
     },
     {
-        title: 'Use Community Tools on Controlled-access Datasets',
-        items: [
+        cardTitle: 'Use Community Tools on Controlled-access Datasets',
+        cardItems: [
             {
-                description: 'Via Dockstore, search from a catalogue of high-quality Docker-based workflows that easily export to platforms such as Terra or Seven Bridges. Explore organization pages to find collections of workflows from labs, institutions, and consortiums or create a page to share your work with the wider bioinformatics community.',
+                platform: 'Dockstore',
+                description: `
+                    Via Dockstore, users can search from a catalogue of high-quality Docker-based workflows that easily export to platforms such as Terra or Seven Bridges.
+                    Explore organization pages to find collections of workflows from labs, institutions, and consortiums or create a page to share your work with the wider bioinformatics community.
+                `,
                 links: {
                     launch: 'https://dockstore.org/',
                     documentation: '',
@@ -63,7 +84,11 @@ const tools = [
                 },
             },
             {
-                description: 'BioData Catalyst powered by HeLx is a web-based platform for collaborative open science. It provides metadata aware data management capabilities with extensive support for sharing and access control. The AppStore also presents tools that can be dynamically launched for each user.',
+                platform: 'HeLx',
+                description: `
+                    HeLx is a web-based platform for collaborative open science. It provides metadata aware data management capabilities with extensive support for sharing and access control.
+                    The AppStore also presents tools that can be dynamically launched for each user.
+                `,
                 links: {
                     launch: 'https://helx.commonsshare.org/',
                     documentation: '',
@@ -120,7 +145,7 @@ const Separator = styled.div`
     }
 `
 
-const ToolsPage = () => (
+const ServicesPage = () => (
     <PageContent width="95%" maxWidth="1080px" center gutters>
         <SEO
             title="Platforms & Services"
@@ -141,17 +166,18 @@ const ToolsPage = () => (
         <Heading>What do you want to do today?</Heading>
 
         {
-            tools.map(tool => (
-                <Card key={ tool.title }>
-                    <CardHeader>{ tool.title }</CardHeader>
+            services.map(service => (
+                <Card key={ service.cardTitle }>
+                    <CardHeader>{ service.cardTitle }</CardHeader>
                     <CardBody>
                         <Grid fluid>
                             <Row gutterWidth={ 0 }>
                                 {
-                                    tool.items.map((item, i) => (
+                                    service.cardItems.map((item, i) => (
                                         <Fragment key={ i }>
                                             <Col xs={ 12 } md={ 5 } style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
-                                                <Paragraph firstLineStyle="font-style: italic; font-size: 120%;">
+                                                <h3 style={{ textAlign: 'center' }}>BioData Catalyst powered by { item.platform }</h3>
+                                                <Paragraph>
                                                     { item.description }
                                                 </Paragraph>
                                                 <ToolLinks>
@@ -163,7 +189,7 @@ const ToolsPage = () => (
                                                 </ToolLinks>
                                             </Col>
                                             {
-                                                i + 1 < tool.items.length&& (
+                                                i + 1 < service.cardItems.length&& (
                                                     <Fragment>
                                                         <Visible md lg xl>
                                                             <Col md={ 2 }>
@@ -191,4 +217,4 @@ const ToolsPage = () => (
     </PageContent>
 )
 
-export default ToolsPage
+export default ServicesPage
