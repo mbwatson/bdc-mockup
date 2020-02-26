da9c7caab7175a8ed94b71b29ca241232a2781ad
Matt Watson <mattwat@gmail.com>
2020-02-26 12:39:53 -0500

    remove email link until email is configured

--- src/pages/accessibility.js
+++ src/pages/accessibility.js
@@ -23,7 +23,6 @@ const AccessibilityPage = () => (
         <Paragraph>
             For questions about our continuing efforts to make the information on the BioData Catalyst ecosystem accessible to all users,
             or to report an accessibility problem on any of our pages, please complete the form on our <Link to ="/contact">Contact page</Link>.
-            If you have difficulty using the form, you may also send e-mail to <a href="mailto:biodatacatalyst@nhlbi.nih.org">biodatacatalyst@nhlbi.nih.org</a>.
         </Paragraph>
 
     </PageContent>
